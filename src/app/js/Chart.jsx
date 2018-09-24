7fa5b0264e935ec44fc5cf909fc70a769c4fef1c
gitgut0110 <jsimms0110@gmail.com>
2018-09-24 14:45:06 +0200

    refresh solved with localstorage

--- src/app/js/Chart.jsx
+++ src/app/js/Chart.jsx
@@ -1,35 +1,56 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { PromiseProvider } from 'mongoose';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-
-const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-];
+import React from "react";
+import { Link } from "react-router-dom";
+import { PromiseProvider } from "mongoose";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
 
 
 const Chart = props => {
-    let chartData=props.data.chart
-    // chartData=
-    console.log(`Chart Component sees: `,chartData)
-        
-        return (
-            <div className="container">
-         <h2>Herea are charts of.... 
-         {props.data.company.companyName}
-         </h2>
-<LineChart width={400} height={400} data={chartData}>
-  <Line type="monotone" dataKey="close" stroke="#8884d8" />
-</LineChart>
-             <img src={props.data.logo.url} alt=""/>
+    console.log(props)
+  let chartData = props.data.chart;
+  console.log(`Chart Component sees: `, chartData);
+if(props.data == ""){
+    return(
+        <div>
+            LOADING...
         </div>
     )
+}else{
+    return (
+        <div className="container">
+      <h2>
+        Herea are charts of....
+        {props.data.company.companyName}
+      </h2>
+      <LineChart width={400} height={400} data={chartData}>
+        <Line type="monotone" dataKey="close" stroke="#8884d8" />
+        {/* <Line type="monotone" dataKey="volume" stroke="#8884d8" /> */}
+        <XAxis dataKey="date" 
+        tickFormatter={formatXAxis}
+        />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <CartesianGrid strokeDasharray="3 3"/>
+        <Tooltip/>
+
+      </LineChart>
+      {/* <img src={props.data.logo.url} alt="" /> */}
+    </div>
+  );
+};
 }
 
-export default Chart
+function formatXAxis(tickItem) {
+    return tickItem.slice(-2)
+    // If using moment.js
+    // return moment(tickItem).format('MMM Do YY')
+    }
+
+export default Chart;
--- src/app/js/Trends.jsx
+++ src/app/js/Trends.jsx
@@ -3,13 +3,22 @@ import { Link } from "react-router-dom";
 import { PromiseProvider } from "mongoose";
 
 const Trends = props => {
-    if(!props.data)this.initstock("SPY")
-      console.log(`Trends Component sees: `, props.data);
+  
+
+  if(props.data == ""){
+    return(
+        <div>
+            LOADING...
+        </div>
+    )
+}else{
   let news = props.data.news;
-  console.log(news);
+  console.log(`Trends Component sees: `, news);
+  // console.log(news);
   const mappedNews = news.map((el,i) => {
     return <li key={i}>{el.headline}</li>;
   });
+
   return (
     <div className="container">
       <h2>
@@ -17,12 +26,11 @@ const Trends = props => {
         {props.data.company.companyName}
       </h2>
       <ul>
-        <li>list item</li>
         {mappedNews}
       </ul>
       <img src={props.data.logo.url} alt="" />
     </div>
   );
 };
-
+}
 export default Trends;
