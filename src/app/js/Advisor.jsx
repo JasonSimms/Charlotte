157c6a0c9ef085ebf698cd66aa1a616c0f3c1e4e
gitgut0110 <jsimms0110@gmail.com>
2018-09-27 19:54:52 +0200

    advisor advances

--- src/app/js/Advisor.jsx
+++ src/app/js/Advisor.jsx
@@ -1,31 +1,59 @@
 import React from "react";
-import { PromiseProvider } from "mongoose";
-import Axios from "axios";
-import api from "./utils/api";
-
 
 class Advisor extends React.Component {
   constructor(props) {
     super(props);
+
+    this._strikeFilter = this._strikeFilter.bind(this);
   }
+
   componentDidMount() {
-    if(this.props.options.underlyingSymbol != this.props.stock)this.props.searchO();
-    console.log(this.props.options, `options data?`)
+    if (this.props.options.underlyingSymbol != this.props.stock)
+      this.props.searchO();
+    console.log(this.props.options, `options data?`);
   }
   render() {
     let content;
-    let optionsTable = "options data Loading"
-//OPTIONS INFO?
+    let earningsDate = "Loading";
+    let mappedCalls = 0;
+
+    //OPTIONS INFO?
     if (this.props.options !== "no options") {
-        let nextEarningsDisplay = "not Available!"
-        let nextEarnings = this.props.options.quote.earningsTimestampStart
-        if(this.props.options.quote.earningsTimestampStart) nextEarningsDisplay = new Date(nextEarnings*1000).toString().slice(0,10)
-        optionsTable = (
-        `We've got info! Next Earnings is 
+      console.log(this.props.options);
+      // Display Earnings Date
+      let nextEarningsDisplay = "not Available!";
+      let nextEarnings = this.props.options.quote.earningsTimestampStart;
+      if (this.props.options.quote.earningsTimestampStart)
+        nextEarningsDisplay = new Date(nextEarnings * 1000)
+          .toString()
+          .slice(0, 10);
+      earningsDate = `We've got info! Next Earnings is 
         ${nextEarningsDisplay}
-        `
-    )
-console.log(this.props.options)}
+        `;
+      // Filter the options
+      // this._strikeFilter();
+      //BUILD THE OPTIONS TABLE
+      let optionsTable = "options data Loading";
+      console.log(this.props.options);
+      let current = this.props.options.quote.ask;
+      let step =
+        this.props.options.options[0].calls[1].strike -
+        this.props.options.options[0].calls[0].strike;
+      let span = 5* step;
+      let calls2 = this.props.options.options[0].calls.filter(
+        el => el.strike < current + span && el.strike > current - span
+      );
+      mappedCalls = calls2.map((el, i) => {
+        return (
+          <tr key={i}>
+            <td>{el.bid} / {el.ask}</td>
+            <td>{el.percentChange}</td>
+            <td>{el.openInterest}</td>
+            <td>{el.strike}</td>
+          </tr>
+        );
+      });
+    }
 
     if (1 > 0) {
       content = "something nice";
@@ -34,16 +62,42 @@ console.log(this.props.options)}
     }
 
     // console.log(this.props)
-    return <div>{content} 
-    <br/><br/>      
-    {this.props.stock}  
-    <br/><br/>  
-    {optionsTable}
-    </div>;
+    return (
+      <div>
+        {content}
+        <br />
+        <br />
+        {this.props.stock}
+        <br />
+        <br />
+        {earningsDate}
+        <h3>Calls for this week</h3>
+        <table>{mappedCalls}</table>
+        <h3>Puts for this week</h3>
+        {/* <table>{mappedPuts}</table> */}
+
+      </div>
+    );
   }
 
-
-  
+  _strikeFilter() {
+    // console.log(calls2);
+    return calls2.map((el, i) => {
+      return "hello";
+      //  <p>this works?</p>
+      {
+        /* <tr key={el.contractSymbol}> */
+      }
+      {
+        /* <td>{el.ask}/{el.bid}</td>
+<td>{el.openInterest}</td>
+<td>{el.percentChange}</td>
+<td>{el.strike}</td> */
+      }
+      // </tr>
+    });
+    // return mappedCalls
+  }
 }
 
 export default Advisor;
